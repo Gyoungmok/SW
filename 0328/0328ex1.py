454c9d502950e1a49d9f1fd343396af48c4feb7f
Gyoungmok <127169438+Gyoungmok@users.noreply.github.com>
2023-04-04 13:17:33 +0900

    Add files via upload

---
+++ 0328/0328ex1.py
@@ -0,0 +1,18 @@
+"""
+2023.03.28
+기경목
+연산자연습
+
+"""
+num1=10 #num1 변수에 정수 10을 저장
+num2=5 #num2 변수에 정수 5를 저장
+
+res1=num1*num2 #res1변수에 num1*num2 결과 저장
+res2=num1/num2 #res2변수에 num1/num2 결과 저장
+res3=num1//num2 #res3변수에 num1//num2 결과 저장
+res4=num1%num2 #res4변수에 num1%num2 결과 저장
+
+print("num1*num2=",res1)
+print("num1/num2=",res2)
+print("num1//num2=",res3)
+print("num1%num2=",res4)
