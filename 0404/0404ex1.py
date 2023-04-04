1366c345483c8a7bede18d3b4c1e80d0f0775a6a
Gyoungmok <127169438+Gyoungmok@users.noreply.github.com>
2023-04-04 13:17:53 +0900

    Add files via upload

---
+++ 0404/0404ex1.py
@@ -0,0 +1,20 @@
+"""
+2023.04.04
+기경목
+표준출력(print)연습
+
+
+"""
+
+print('한국','korea',2023)
+print('한국','korea',2023,sep='-')
+print('한국','korea',2023,end='')
+print() #한줄 띄우기
+print('python','language');print("Hi python")
+print('python','language',end='');print("Hi python")
+print("Hi python")
+print("'Hi python'")#작은 따옴표 출력
+print("\'Hi python\'")
+print("python "*5)
+print("hi \n phtyon")
+print("\n 포맷 활용")
---
+++ 0404/0404ex2.py
@@ -0,0 +1,10 @@
+'''
+2023.04.04
+기경목
+표준출력(print) format()연습
+
+'''
+print('이름은 {}이고 나이는 {}입니다.'.format('mklee',25))
+print('이름 {name}, 나이{age}, 주소{add}'.format(add='Busan',name='기경목',age='20'))
+print('The lucky number is ({:14})'.format(7777))#숫자 기본 오른쪽 정렬
+print('The lucky number is({:<14})'.format(7777))
